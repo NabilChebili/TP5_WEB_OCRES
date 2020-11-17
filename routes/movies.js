26bab885e793ec77b3ccdb257a6991de34c20c56
NabilChebili <nabilchebili@ymail.com>
2020-11-17 15:31:19 +0100

    Revert "step 3"
    This reverts commit 703fd014fa4106835c8d45b2cdbabdd83905df7b.

--- routes/movies.js
+++ routes/movies.js
@@ -46,7 +46,7 @@ router.put('/', (req, res) => {
             actors : response.data.Actors,
             poster: response.data.Poster,
             boxOffice: response.data.BoxOffice,
-            rottenTomatoesScore: 0, //_.find(response.data.Ratings,["Source","Rotten Tomatoes"]).Value,
+            rottenTomatoesScore: _.find(response.data.Ratings,["Source","Rotten Tomatoes"]).Value,
         } 
     ) } );
     
